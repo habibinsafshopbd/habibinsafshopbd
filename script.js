4f9ffb993ca309015409bf49e7ead08b6ec3064e
habibinsafshopbd <abdullahhabib2122@gmail.com>
2026-07-18 12:22:47 +0600

    Add menuOpen function to toggle menu display

---
+++ script.js
@@ -0,0 +1,9 @@
+function menuOpen() {
+    var menu = document.getElementById("menu");
+
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
